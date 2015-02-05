14a1e27215078dfb6c171635fbcafa26fb11553b
duccioa <duccio.aiazzi@gmail.com>
2015-02-05 11:09:02 +0100

    First commit of plot1

---
+++ plot1.R
@@ -0,0 +1,21 @@
+if(!file.exists("household_power_consumption.txt")) {
+      download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.zip", method = "curl")
+unzip("household_power_consumption.zip")
+}
+if(!exists("hpc_sub")) {
+      hpc <- read.table("household_power_consumption.txt", sep = ";", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), stringsAsFactors = FALSE)
+      hpc$Time <- strptime(paste(hpc$Date, hpc$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+      hpc <- hpc[,2:9]
+      hpc_sub <- subset(hpc, format(Time, "%Y-%m-%d") %in% c("2007-02-01", "2007-02-02"))
+      rm(hpc)
+      for(i in 2:8){hpc_sub[,i]<-as.numeric(hpc_sub[,i])}
+      hpc_sub <- cbind("Date_time" = hpc_sub[,1], hpc_sub[,2:8])
+}
+
+
+#plot1
+png("plot1.png", width = 480, height = 480)
+hist(hpc_sub$Global_active_power, xlab = "Global active power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red")
+dev.off()
+
+
